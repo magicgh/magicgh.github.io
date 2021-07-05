1abdd75b53df0a5b4a1f12850f46253ca6082cb6
magicgh <magicgher@hotmail.com>
2021-07-05 12:09:17 +0800

    :recycle: Refactor

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,26 @@
-const config = require('./config');
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
 
 module.exports = {
-  siteMetadata: {
-    title: config.siteTitle,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: config.manifestName,
-        short_name: config.manifestShortName,
-        start_url: config.manifestStartUrl,
-        background_color: config.manifestBackgroundColor,
-        theme_color: config.manifestThemeColor,
-        display: config.manifestDisplay,
-        icon: config.manifestIcon, // This path is relative to the root of the site.
-        icon_options: {
-          purpose: `maskable`,
-        }
-      },
-    },
-    {
-      resolve: "gatsby-theme-minimalist",
-      options: {
-        // SEO
-        title: config.siteTitle,
-        description: "Your site description",
-        siteUrl: "https://magicgh.com",
-        appName: config.manifestName,
-
-        // Content
-        headline: "Your headline",
-        subHeadline: "Your <strong>meaningful</strong> message.<br/>",
-        socialList: [{
-          icon: 'FaGithub', // a detailed explanation can be found in the options section
-          url: 'https://github.com/magicgh',
-          ariaLabel: 'Link to my GitHub profile',
+    /* Your site config here */
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Magicgh - Homepage`,
+                short_name: `Magicgh`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `standalone`,
+                icon: `static/favicon.png`,
+                icon_options: {
+                    purpose: `maskable`,
+                },
+            },
         },
-          {
-            icon: 'FaLinkedin',
-            url: 'https://linkedin.com/in/xuan-z',
-            ariaLabel: 'Link to my Linkedin'
-          }
-        ]
-      }
-    },
-    'gatsby-plugin-offline',
-  ],
+    ],
 };
